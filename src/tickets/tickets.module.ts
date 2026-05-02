4cc45ef52f6262019523c688c30aa23ff03f05e3
arman573 <arman@harmoniq.se>
2026-05-02 14:36:46 +0200

    Register intelligence services and entities

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -3,12 +3,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './message.entity';
 import { Ticket } from './ticket.entity';
 import { Customer } from './customer.entity';
+import { CustomerEvent } from './customer-event.entity';
+import { CustomerFact } from './customer-fact.entity';
+import { CustomerProfile } from './customer-profile.entity';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
+import { CustomerIntelligenceService } from './customer-intelligence.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket, Message, Customer])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Ticket,
+      Message,
+      Customer,
+      CustomerEvent,
+      CustomerFact,
+      CustomerProfile,
+    ]),
+  ],
   controllers: [TicketsController],
-  providers: [TicketsService],
+  providers: [TicketsService, CustomerIntelligenceService],
 })
 export class TicketsModule {}
