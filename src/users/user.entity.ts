83b034286461e54e70028bfd4dcd5d84da60050f
arman573 <arman@harmoniq.se>
2026-05-02 11:53:37 +0200

    Add user roles

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,10 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -10,6 +15,13 @@ export class User {
 
   @Column()
   email!: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role!: UserRole;
 
   @Column({ nullable: true, select: false })
   password?: string;
