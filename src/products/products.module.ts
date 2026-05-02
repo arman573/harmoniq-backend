30f3c31eb2509f3a601b3ddcf960fb002e8e2d2d
arman573 <arman@harmoniq.se>
2026-05-02 20:31:54 +0200

    Add products module

---
+++ src/products/products.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { ProductSpec } from './product-spec.entity';
+import { ProductTag } from './product-tag.entity';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Product, ProductSpec, ProductTag])],
+  providers: [ProductsService],
+  controllers: [ProductsController],
+})
+export class ProductsModule {}
