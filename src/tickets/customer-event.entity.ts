b030f30c12ec03be915eb6fcd922739fa86aeca2
arman573 <arman@harmoniq.se>
2026-05-02 14:23:35 +0200

    Add customer event entity

---
+++ src/tickets/customer-event.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Customer } from './customer.entity';
+
+@Entity()
+export class CustomerEvent {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => Customer, { onDelete: 'CASCADE' })
+  customer!: Customer;
+
+  @Column()
+  type!: string;
+
+  @Column({ type: 'jsonb', nullable: true })
+  payload?: Record<string, unknown>;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
