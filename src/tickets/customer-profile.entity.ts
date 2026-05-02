e1ee7fbf0abcf1c1914968d6fc02dec3fd5fc648
arman573 <arman@harmoniq.se>
2026-05-02 14:21:25 +0200

    Add customer profile entity

---
+++ src/tickets/customer-profile.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Customer } from './customer.entity';
+
+@Entity()
+export class CustomerProfile {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @OneToOne(() => Customer, (customer) => customer.profile, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  customer!: Customer;
+
+  @Column({ type: 'jsonb', default: {} })
+  attributes!: Record<string, unknown>;
+
+  @Column({ type: 'text', nullable: true })
+  notes?: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
