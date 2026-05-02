5622d419673d98b2b0a404f5abb279752092e371
arman573 <arman@harmoniq.se>
2026-05-02 16:12:03 +0200

    Wire customer profile endpoint into tickets module

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from '../customers/customer.entity';
+import { CustomersController } from '../customers/customers.controller';
+import { CustomersService } from '../customers/customers.service';
+import { CustomerEvent } from '../intelligence/customer-event.entity';
+import { CustomerFact } from '../intelligence/customer-fact.entity';
 import { Message } from './message.entity';
 import { Ticket } from './ticket.entity';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket, Message])],
-  controllers: [TicketsController],
-  providers: [TicketsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Ticket,
+      Message,
+      Customer,
+      CustomerEvent,
+      CustomerFact,
+    ]),
+  ],
+  controllers: [TicketsController, CustomersController],
+  providers: [TicketsService, CustomersService],
 })
 export class TicketsModule {}
