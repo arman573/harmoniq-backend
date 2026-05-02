1ebcb5be07f25e148a4f728f2da0b05d11b575fb
arman573 <arman@harmoniq.se>
2026-05-02 16:18:52 +0200

    Add customer profile controller

---
+++ src/tickets/customers.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TicketsService } from './tickets.service';
+
+@UseGuards(AuthGuard('jwt'))
+@Controller('customers')
+export class CustomersController {
+  constructor(private readonly ticketsService: TicketsService) {}
+
+  @Get(':id/profile')
+  getProfile(@Param('id', ParseIntPipe) id: number) {
+    return this.ticketsService.getCustomerProfile(id);
+  }
+}
