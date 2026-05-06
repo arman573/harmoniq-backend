e7faa178eb4068ef3272f711b7c5f5492986ba90
arman573 <arman@harmoniq.se>
2026-05-06 18:06:11 +0200

    Register ProductAnalysis repository

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { ProductSpec } from './product-spec.entity';
 import { ProductTag } from './product-tag.entity';
+import { ProductAnalysis } from './product-analysis.entity';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductSpec, ProductTag])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Product,
+      ProductSpec,
+      ProductTag,
+      ProductAnalysis,
+    ]),
+  ],
   providers: [ProductsService],
   controllers: [ProductsController],
 })
