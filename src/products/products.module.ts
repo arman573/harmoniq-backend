9e863778bc7387cad2729d7c115465a606cfd95a
arman573 <arman@harmoniq.se>
2026-06-15 17:09:07 +0200

    Wire product analysis into products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExplainabilityModule } from '../explainability/explainability.module';
+import { IngredientsModule } from '../ingredients/ingredients.module';
 import { Product } from './product.entity';
 import { ProductSpec } from './product-spec.entity';
 import { ProductTag } from './product-tag.entity';
 import { ProductAnalysis } from './product-analysis.entity';
+import { ProductAnalysisService } from './product-analysis.service';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
 @Module({
   imports: [
+    ExplainabilityModule,
+    IngredientsModule,
     TypeOrmModule.forFeature([
       Product,
       ProductSpec,
@@ -16,7 +21,7 @@ import { ProductsController } from './products.controller';
       ProductAnalysis,
     ]),
   ],
-  providers: [ProductsService],
+  providers: [ProductsService, ProductAnalysisService],
   controllers: [ProductsController],
 })
 export class ProductsModule {}
