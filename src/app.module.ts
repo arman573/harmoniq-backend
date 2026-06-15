6369714082002633d01b7e1c8ee39f3e655ea2ab
arman573 <arman@harmoniq.se>
2026-06-15 16:20:17 +0200

    Wire ingredient module into current backend app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { Customer } from './customers/customer.entity';
 import { Ingredient } from './ingredients/ingredient.entity';
 import { IngredientsModule } from './ingredients/ingredients.module';
-import { Product } from './products/product.entity';
+import { CustomerEvent } from './intelligence/customer-event.entity';
+import { CustomerFact } from './intelligence/customer-fact.entity';
 import { ProductAnalysis } from './products/product-analysis.entity';
 import { ProductSpec } from './products/product-spec.entity';
 import { ProductTag } from './products/product-tag.entity';
+import { Product } from './products/product.entity';
 import { ProductsModule } from './products/products.module';
-import { Customer } from './tickets/customer.entity';
+import { TaxonomyCategory } from './taxonomy/taxonomy-category.entity';
+import { TaxonomySpec } from './taxonomy/taxonomy-spec.entity';
+import { TaxonomySuggestion } from './taxonomy/taxonomy-suggestion.entity';
+import { TaxonomyTag } from './taxonomy/taxonomy-tag.entity';
+import { TaxonomyModule } from './taxonomy/taxonomy.module';
 import { CustomerChatConversation } from './tickets/customer-chat-conversation.entity';
 import { CustomerChatInternalNote } from './tickets/customer-chat-internal-note.entity';
 import { CustomerChatMessage } from './tickets/customer-chat-message.entity';
-import { CustomerEvent } from './tickets/customer-event.entity';
-import { CustomerFact } from './tickets/customer-fact.entity';
 import { CustomerProfile } from './tickets/customer-profile.entity';
 import { Message } from './tickets/message.entity';
 import { Ticket } from './tickets/ticket.entity';
 import { TicketsModule } from './tickets/tickets.module';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
-import { TaxonomyCategory } from './taxonomy/taxonomy-category.entity';
-import { TaxonomySpec } from './taxonomy/taxonomy-spec.entity';
-import { TaxonomyTag } from './taxonomy/taxonomy-tag.entity';
-import { TaxonomySuggestion } from './taxonomy/taxonomy-suggestion.entity';
-import { TaxonomyModule } from './taxonomy/taxonomy.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
