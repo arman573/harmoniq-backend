dd0df95e3e4f9b2d41b9af37069e1985f19017ad
arman573 <arman@harmoniq.se>
2026-09-22 17:19:53 +0200

    mail-agent: register GPT workstyle services

--- src/mail-agent/mail-agent.module.ts
+++ src/mail-agent/mail-agent.module.ts
@@ -3,11 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailAgentMessage } from './mail-agent-message.entity';
 import { MailAgentController } from './mail-agent.controller';
 import { MailAgentService } from './mail-agent.service';
+import { MailAgentGptHelpService } from './mail-agent-gpt-help.service';
+import { MailAgentWorkstyle } from './mail-agent-workstyle.entity';
+import { MailAgentWorkstyleService } from './mail-agent-workstyle.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MailAgentMessage])],
+  imports: [TypeOrmModule.forFeature([MailAgentMessage, MailAgentWorkstyle])],
   controllers: [MailAgentController],
-  providers: [MailAgentService],
-  exports: [MailAgentService],
+  providers: [MailAgentService, MailAgentGptHelpService, MailAgentWorkstyleService],
+  exports: [MailAgentService, MailAgentWorkstyleService],
 })
 export class MailAgentModule {}
