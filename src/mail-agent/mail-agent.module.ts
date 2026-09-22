7122c74c5f097ab599b00dcb6b2459ea1eb086ad
arman573 <arman@harmoniq.se>
2026-09-22 15:34:14 +0200

    mail-agent: register message repository

--- src/mail-agent/mail-agent.module.ts
+++ src/mail-agent/mail-agent.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailAgentMessage } from './mail-agent-message.entity';
 import { MailAgentController } from './mail-agent.controller';
 import { MailAgentService } from './mail-agent.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MailAgentMessage])],
   controllers: [MailAgentController],
   providers: [MailAgentService],
   exports: [MailAgentService],
